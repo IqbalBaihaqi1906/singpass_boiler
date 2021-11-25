20f90ed2bac9dbe815384d0560a4a8456af40205
IqbalBaihaqi1906 <iqballbaihaqi1906@gmail.com>
2021-11-25 12:19:18 +0700

    setup frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>SINGPASS BOILER PLATE</h1>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+
+
+      <button>Get Info</button>
     </div>
   );
 }
